235772813bad76fb60d695e7ea688670142cb7cd
Asthasachan <46759177+Asthasachan@users.noreply.github.com>
2021-04-12 16:45:00 +0530

    adding api for blood bank location search

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+var axios = require("axios").default;
 const ejs = require("ejs");
 
 const app = express();
@@ -21,6 +22,40 @@ app.get("/register", function(req, res){
 app.get("/find", function(req,res){
     res.render("find");
 });
+
+
+app.post("/find", function(req,res){
+    const query=req.body.cityname;
+    var options = {
+        method: 'GET',
+        url: 'https://trueway-places.p.rapidapi.com/FindPlaceByText',
+       // params: {text: 'blood bank, saket nagar, kanpur', language: 'en'},
+       params: {text: 'blood bank,'+query, language: 'en'},
+        headers: {
+          'x-rapidapi-key': '55e0ca442bmshbbc6b2ccd48264ap1ace4fjsn61865348c81f',
+          'x-rapidapi-host': 'trueway-places.p.rapidapi.com'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          const detail=response.data;
+          res.render("search", {
+            n:"Name: ",
+            name: detail.results[0].name,
+            ad:"Address: ",
+            addr: detail.results[0].address,
+            la:"Latitude: ",
+            lat: detail.results[0].location.lat,
+            lo:"Longitude: ",
+            long: detail.results[0].location.lng,
+          });
+      }).catch(function (error) {
+          console.error(error);
+      });
+      
+    });
+
 
 app.get("/more", function(req,res){
     res.render("more");
